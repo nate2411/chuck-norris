66be355f28e403270c56677f6e6634ded61287c8
nate2411 <Nathandj28@gmail.com>
2021-07-21 14:01:15 +0200

    update

--- venv/app7.py
+++ venv/app7.py
@@ -8,3 +8,8 @@ def get_chuck_norris_jokes():
     api_url = "https://api.chucknorris.io/jokes/random"
     response = requests.get(api_url).json()
     return "<strong>Random joke from chuck norris: </strong>" + response['value']
+
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
